37814e5a39592a945c24a8c315197a596cdeeecc
Alperen Tekin <121463341+sirALp@users.noreply.github.com>
2024-03-21 16:35:06 +0300

    removed firebaseio files that causes bugs

--- js/script.js
+++ js/script.js
@@ -9,20 +9,6 @@ $(function () {
   });
 });
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC3GYyOhU6pjgYaeJIJ-KXyS-65PnKdK5Q",
-  authDomain: "sistem-analiz-proje.firebaseapp.com",
-  projectId: "sistem-analiz-proje",
-  storageBucket: "sistem-analiz-proje.appspot.com",
-  messagingSenderId: "27776536874",
-  appId: "1:27776536874:web:684ea0a88d59b856aa4e53",
-  measurementId: "G-H60XL19L4C",
-};
-
-// Initialization Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 (function (global) {
   var tekinrest = {};
 
@@ -259,12 +245,3 @@ const analytics = getAnalytics(app);
   global.switchMenuButtonToActive = switchMenuButtonToActive;
   global.$tekinrest = tekinrest;
 })(window);
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
